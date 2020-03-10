a1fef5902918d1195589bc34afe90ee14875dafc
ivladu <ivladu@IVLADU-RO.ro.oracle.com>
2020-03-10 14:01:46 +0200

    Change logging from TCP to UDP

--- oci_services.py
+++ oci_services.py
@@ -33,7 +33,7 @@ logging_address = os.environ['LOGGING_ADDRESS']
 logging_port = os.environ['LOGGING_PORT']
 
 # log to CSA VM on TCP
-syslog = SysLogHandler(address=(logging_address, logging_port), socktype=socket.SOCK_STREAM)
+syslog = SysLogHandler(address=(logging_address, int(logging_port)))
 format = f'%(asctime)s {app_name}: %(levelname)s : %(lineno)d : %(message)s'
 formatter = logging.Formatter(format, datefmt='%b %d %H:%M:%S')
 syslog.setFormatter(formatter)
