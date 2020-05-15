5a750d930802fb1bb61dbbbfa1eff9c09805f198
ivladu <ivladu@IVLADU-RO.ro.oracle.com>
2020-05-15 14:25:07 +0300

    add report_no to path history tables

--- oci_services.py
+++ oci_services.py
@@ -964,20 +964,20 @@ class DBSystem(object):
          write_file( data, 'autonomous_db' )
          
          # DB Home Patch History
-         data = 'action, id, lifecycle_details, lifecycle_state, patch_id, time_ended, time_started'
+         data = 'action, id, lifecycle_details, lifecycle_state, patch_id, time_ended, time_started, report_no'
 
          for db_home_patch in self.db_home_patch_history:
             data += '\n'
-            data += f'{db_home_patch.action}, {db_home_patch.id}, {db_home_patch.lifecycle_details}, {db_home_patch.lifecycle_state}, {db_home_patch.patch_id}, {db_home_patch.time_ended}, {db_home_patch.time_started},'
+            data += f'{db_home_patch.action}, {db_home_patch.id}, {db_home_patch.lifecycle_details}, {db_home_patch.lifecycle_state}, {db_home_patch.patch_id}, {db_home_patch.time_ended}, {db_home_patch.time_started}, {report_no}'
 
          write_file( data, 'db_home_patch_history' )
          
          # DB System Patch History
-         data = 'action, id, lifecycle_details, lifecycle_state, patch_id, time_ended, time_started'
+         data = 'action, id, lifecycle_details, lifecycle_state, patch_id, time_ended, time_started, report_no'
 
          for db_sys_patch in self.db_system_patch_history:
             data += '\n'
-            data += f'{db_sys_patch.action}, {db_sys_patch.id}, {db_sys_patch.lifecycle_details}, {db_sys_patch.lifecycle_state}, {db_sys_patch.patch_id}, {db_sys_patch.time_ended}, {db_sys_patch.time_started},'
+            data += f'{db_sys_patch.action}, {db_sys_patch.id}, {db_sys_patch.lifecycle_details}, {db_sys_patch.lifecycle_state}, {db_sys_patch.patch_id}, {db_sys_patch.time_ended}, {db_sys_patch.time_started}, {report_no}'
 
          write_file( data, 'db_sys_patch_history' )
          
